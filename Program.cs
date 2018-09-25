dbe50dbe48a2b6bff7876d32b992389370f1c7a7
OscarAbrahamH <43403654+OscarAbrahamH@users.noreply.github.com>
2018-09-24 19:45:42 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class PROGRAMA2
+{
+    public static void Main()
+    {
+        string TEXTO = "OSCAR ABRAHAM HUERTA MADUEÑO";
+        string Final = "";
+        string Palabra = "";
+
+        foreach (char Letra in TEXTO)
+        {
+            if (Letra != ' ')
+                Palabra += Letra;
+            else
+            {
+                Final = " " + Palabra + Final;
+                Palabra = "";
+            }
+        }
+        Final = Palabra + Final;
+        Console.WriteLine(Final); 
+        Console.ReadKey();
+    }
+}
